c966f97e4389d1e7f7ed2b778ef2820289646e72
R Nat <ryan@txnataluks.net>
2018-01-29 15:43:29 -0600

    Update coinbase.py

--- plugins/coinbase.py
+++ plugins/coinbase.py
@@ -1,7 +1,7 @@
 def coinbase(self, message, coin):
     coin = coin.upper().replace(" ", "")
     if coin not in ('LTC', 'BTC', 'ETH'):
-        self.reply(message, "Please specify BTC, LTC, or ETH.", True)
+        self.send_message(message, "Please specify BTC, LTC, or ETH.", True)
     else:
         try:
             url = "https://api.coinbase.com/v2/prices/%s-USD/spot" % coin
@@ -10,4 +10,4 @@ def coinbase(self, message, coin):
             msg = "%s is currently at $%s." % (coin, price)
         except:
             msg = "Sorry there was a problem retrieving the information."
-        self.reply(message, msg, True)  
+        self.send_message(message, msg, True)  
