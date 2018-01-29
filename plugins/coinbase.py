2149de33cd56cb1154165791c435924b37053e6d
R Nat <ryan@txnataluks.net>
2018-01-29 17:20:35 -0600

    Update coinbase.py

--- plugins/coinbase.py
+++ plugins/coinbase.py
@@ -2,6 +2,7 @@ import requests
 import sys
 
 def coinbase(bot, update, coin):
+    ##"""/coinbase <coin>: Show price of selected cyrptocurrency"""
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     coin = coin.upper().replace(" ", "")
     if coin not in ('LTC', 'BTC', 'ETH'):
@@ -16,15 +17,15 @@ def coinbase(bot, update, coin):
             msg = "Sorry there was a problem retrieving the information."
         bot.send_message(chat_id=update.message.chat_id, msg, True) 
         
-def coinbase_cmd(bot, update, args):
-    """/coinbase <coin>: Show price of selected cyrptocurrency"""
-    if args is not None and len(args) > 0:
-        coin = args[0]
-    try:
-        ##print(coin)
-        amount_int = int(coin)
-        msg = coinbase(bot, update, coin)
-    except BaseException as e:
-        print(e)
-        msg = "Please use integers greater than 0"
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+##def coinbase_cmd(bot, update, args):
+##    """/coinbase <coin>: Show price of selected cyrptocurrency"""
+##    if args is not None and len(args) > 0:
+##        coin = args[0]
+##    try:
+##        print(coin)
+##        amount_int = int(coin)
+##        msg = coinbase(bot, update, coin)
+##    except BaseException as e:
+##        print(e)
+##        msg = "Please use integers greater than 0"
+##    bot.send_message(chat_id=update.message.chat_id, text=msg)
